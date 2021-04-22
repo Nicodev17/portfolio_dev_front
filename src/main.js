6aa467a2b174741f7cbddaa31cb29e7167b18e7b
Nicolas Sartelet <nicolas.sartelet.mmi@gmail.com>
2021-04-22 23:50:17 +0200

    Création du menu
    Le menu de navigation a été créé, avec un bouton et un déploiement animés, la section home a aussi été initialisée.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log('ok');
+// Event Menu
+const boxHamburger = document.querySelector('#box-hamburger');
+const menu = document.querySelector('#menu');
+
+boxHamburger.addEventListener('click', event => {
+    event.target.classList.toggle('active');
+    menu.classList.toggle('visible');
+})
