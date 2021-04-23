c840140c2f55ec6a759c770d0704dd8a33dea29a
Nicolas Sartelet <nicolas.sartelet.mmi@gmail.com>
2021-04-23 23:16:36 +0200

    Avancée header et home
    Le header est terminé, la section home est a 80%

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 // Event Menu
 const boxHamburger = document.querySelector('#box-hamburger');
 const menu = document.querySelector('#menu');
+const liens = document.querySelectorAll('#container-liens a');
+const logo = document.querySelector('#logo circle');
 
 boxHamburger.addEventListener('click', event => {
     event.target.classList.toggle('active');
     menu.classList.toggle('visible');
-})
+});
+
+for (let i = 0; i < liens.length; i++) {
+    liens[i].addEventListener("mouseover", function() {
+      liens[i].classList.toggle("lien-actif");
+    });
+
+    liens[i].addEventListener("mouseleave", function() {
+        liens[i].classList.toggle("lien-actif");
+    });
+}
+
+window.addEventListener('load', event => {
+    logo.classList.add('visible');
+});
