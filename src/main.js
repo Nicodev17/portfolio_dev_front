f17fe47d5d2fff0b3db90593f2568695b0e89d25
Nicolas Sartelet <nicolas.sartelet.mmi@gmail.com>
2021-04-28 00:09:22 +0200

    Header / home terminés
    Le header, le home du site et le menu sont terminés.
    
    La section About est commencée.

--- src/main.js
+++ src/main.js
@@ -1,24 +1,54 @@
+// Paralax avec Rellax.js
+const rellax = new Rellax('.rellax');
+
 // Event Menu
+const body = document.querySelector('body');
 const boxHamburger = document.querySelector('#box-hamburger');
 const menu = document.querySelector('#menu');
 const liens = document.querySelectorAll('#container-liens a');
+const traitLien = document.querySelector('.trait');
 const logo = document.querySelector('#logo circle');
+const vagues = document.querySelector('#vagues');
+const title = document.querySelector('h1');
 
+// Box 3 traits
 boxHamburger.addEventListener('click', event => {
     event.target.classList.toggle('active');
     menu.classList.toggle('visible');
+    if(menu.classList.contains('visible')) {
+        body.style.overflow = 'hidden';
+    } else {
+        body.style.overflow = 'visible';
+    };
 });
 
+// Event des liens
 for (let i = 0; i < liens.length; i++) {
-    liens[i].addEventListener("mouseover", function() {
+    liens[i].addEventListener("mouseover", event => {
       liens[i].classList.toggle("lien-actif");
     });
 
-    liens[i].addEventListener("mouseleave", function() {
+    liens[i].addEventListener("mouseleave", event => {
         liens[i].classList.toggle("lien-actif");
+    });
+
+    liens[i].addEventListener("click", event => {
+        body.style.overflow = 'visible';
+        traitLien.style.opacity= '0';
+        // Décalage du repli du menu pour éviter le bug visuel
+        setTimeout(() => {
+            boxHamburger.classList.toggle('active');
+            menu.classList.toggle('visible');
+        }, 100);
+        setTimeout(() => {
+            traitLien.style.opacity = '1';
+        }, 2000);
     });
 }
 
+// Pour l'animation du home
 window.addEventListener('load', event => {
     logo.classList.add('visible');
+    vagues.classList.add('visible');
+    title.classList.add('visible');
 });
